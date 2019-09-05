4c17dc0360119bc46783f4e9d70ccd011b004304
Elie C <ecc226@nau.edu>
2019-09-05 13:57:08 -0700

    Update game.js

--- game.js
+++ game.js
@@ -4,3 +4,25 @@ var renderer = PIXI.autoDetectRenderer(400, 400. {backgroundColor: 0x000033});
 gameport.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
+
+var bill = new PIXI.Sprite(PIXI.Texture.fromImage("bill_cipher.png"));
+var circle = new PIXI.Sprite(PIXI.Texture.fromImage("circle_dude.png"));
+var square = new PIXI.Sprite(PIXI.Texture.fromImage("Square_Man.png"));
+var star = new PIXI.Sprite(PIXI.Texture.fromImage("Star_Guy.png"));
+
+stage.addChild(bill);
+stage.addChild(circle);
+stage.addChild(square);
+stage.addChild(star);
+
+bill.position.x = 0;
+bill.position.y = 0;
+
+circle.position.x = 30;
+circle.position.y = 30;
+
+square.positon.x = 370;
+square.position.y = 30;
+
+star.position.x = 370;
+star.position.y = 0;
