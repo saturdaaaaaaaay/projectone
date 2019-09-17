b69959008d344f11759198f45baeb189f9624402
saturdaaaaaaaay <ecc226@nau.edu>
2019-09-17 10:23:50 -0700

    Added movement to playable character

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var gameport = document.getElementById("gameport");
 
-var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0x000033});
+var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0xe6b5eb});
 gameport.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
@@ -24,20 +24,43 @@ bill.position.y = 0;
 playground.addChild(star);
 star.anchor.x = 0.5;
 star.anchor.y = 0.5;
-star.position.x = 20;
-star.position.y = -30;
+star.position.x = -150;
+star.position.y = -50;
 
 playground.addChild(circle);
 circle.anchor.x = 0.5;
 circle.anchor.y = 0.5;
-circle.position.x = 40;
-circle.position.y = 30;
+circle.position.x = 80;
+circle.position.y = 150;
 
 playground.addChild(square);
 square.anchor.x = 0.5;
 square.anchor.y = 0.5;
-square.position.x = 35;
-square.position.y = -45;
+square.position.x = 100;
+square.position.y = -100;
+
+function keydownEventHandler(e) {
+
+  var key = e.keyCode;
+
+  if (key == 87 || key == 38) { // W key
+    bill.position.y -= 5;
+  }
+
+  if (key == 83 || key == 40) { // S key
+    bill.position.y += 5;
+  }
+
+  if (key == 65 || key == 37) { // A key
+    bill.position.x -= 5;
+  }
+
+  if (key == 68 || key == 39) { // D key
+    bill.position.x += 5;
+  }
+}
+
+document.addEventListener('keydown', keydownEventHandler);
 
 function animate() {
   requestAnimationFrame(animate);
