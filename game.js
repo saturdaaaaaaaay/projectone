be8106116fdce929e502dda2db250893e20adbb1
saturdaaaaaaaay <ecc226@nau.edu>
2019-09-17 10:10:50 -0700

    Display successful
    Fixed game.js and replaced pixi.js so that game could finally be displayed online

--- game.js
+++ game.js
@@ -1,40 +1,47 @@
 var gameport = document.getElementById("gameport");
 
-var renderer = PIXI.autoDetectRenderer(400, 400. {backgroundColor: 0x000033});
+var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0x000033});
 gameport.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
+
+var star = new PIXI.Sprite(PIXI.Texture.fromImage("Star_Guy.png"));
+var square = new PIXI.Sprite(PIXI.Texture.fromImage("Square_Man.png"));
+var circle = new PIXI.Sprite(PIXI.Texture.fromImage("circle_dude.png"));
+var bill = new PIXI.Sprite(PIXI.Texture.fromImage("bill_cipher.png"));
 
 var playground = new PIXI.Container();
 playground.position.x = 200;
 playground.position.y = 200;
 stage.addChild(playground);
 
-var bill = new PIXI.Sprite(PIXI.Texture.fromImage("bill_cipher.png"));
-var circle = new PIXI.Sprite(PIXI.Texture.fromImage("circle_dude.png"));
-var square = new PIXI.Sprite(PIXI.Texture.fromImage("Square_Man.png"));
-var star = new PIXI.Sprite(PIXI.Texture.fromImage("Star_Guy.png"));
-
 playground.addChild(bill);
+bill.anchor.x = 0.5;
+bill.anchor.y = 0.5;
 bill.position.x = 0;
 bill.position.y = 0;
 
+playground.addChild(star);
+star.anchor.x = 0.5;
+star.anchor.y = 0.5;
+star.position.x = 20;
+star.position.y = -30;
+
 playground.addChild(circle);
-circle.position.x = 30;
+circle.anchor.x = 0.5;
+circle.anchor.y = 0.5;
+circle.position.x = 40;
 circle.position.y = 30;
 
 playground.addChild(square);
-square.positon.x = 370;
-square.position.y = 30;
-
-playground.addChild(star);
-star.position.x = 370;
-star.position.y = 0;
+square.anchor.x = 0.5;
+square.anchor.y = 0.5;
+square.position.x = 35;
+square.position.y = -45;
 
 function animate() {
   requestAnimationFrame(animate);
 
   renderer.render(stage);
 }
-
 animate();
