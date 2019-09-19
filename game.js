57266072e1c4e2ac6b4c1e262fae84542edc35e3
saturdaaaaaaaay <ecc226@nau.edu>
2019-09-19 15:06:24 -0700

    Update game.js
    Removed playground container and made Square sprite move when Bill comes close to it

--- game.js
+++ game.js
@@ -10,36 +10,39 @@ var square = new PIXI.Sprite(PIXI.Texture.fromImage("Square_Man.png"));
 var circle = new PIXI.Sprite(PIXI.Texture.fromImage("circle_dude.png"));
 var bill = new PIXI.Sprite(PIXI.Texture.fromImage("bill_cipher.png"));
 
+/*
 var playground = new PIXI.Container();
 playground.position.x = 200;
 playground.position.y = 200;
 stage.addChild(playground);
+*/
 
-playground.addChild(bill);
-bill.anchor.x = 0.5;
-bill.anchor.y = 0.5;
-bill.position.x = 0;
-bill.position.y = 0;
+stage.addChild(bill);
+//bill.anchor.x = 0.5;
+//bill.anchor.y = 0.5;
+bill.position.x = 200;
+bill.position.y = 200;
 
-playground.addChild(star);
-star.anchor.x = 0.5;
-star.anchor.y = 0.5;
+stage.addChild(star);
+//star.anchor.x = 0.5;
+//star.anchor.y = 0.5;
 star.position.x = -150;
 star.position.y = -50;
 
-playground.addChild(circle);
-circle.anchor.x = 0.5;
-circle.anchor.y = 0.5;
+stage.addChild(circle);
+//circle.anchor.x = 0.5;
+//circle.anchor.y = 0.5;
 circle.position.x = 80;
 circle.position.y = 150;
 
-playground.addChild(square);
-square.anchor.x = 0.5;
-square.anchor.y = 0.5;
+stage.addChild(square);
+//square.anchor.x = 0.5;
+//square.anchor.y = 0.5;
 square.position.x = 100;
-square.position.y = -100;
+square.position.y = 100;
 
-function keydownEventHandler(e) {
+function keydownEventHandler(e)
+{
 
   var key = e.keyCode;
 
@@ -62,8 +65,24 @@ function keydownEventHandler(e) {
 
 document.addEventListener('keydown', keydownEventHandler);
 
-function animate() {
+function animate()
+{
   requestAnimationFrame(animate);
+
+  document.getElementById("billpos").innerHTML = "Bill position: " + bill.position.x + ", " + bill.position.y;
+
+  var buffer = 25;
+
+  if (bill.position.x >= square.position.x - buffer &&
+    bill.position.x <= square.position.x + buffer &&
+    bill.position.y >= square.position.y - buffer &&
+    bill.position.y <= square.position.y + buffer)
+  {
+    square.position.x = Math.floor(Math.random() * 300) + 50;
+    square.position.y = Math.floor(Math.random() * 300) + 50;
+
+    document.getElementById("squarepos").innerHTML += "\nSquare position: " + square.position.x + ", " + square.position.y;
+  }
 
   renderer.render(stage);
 }
