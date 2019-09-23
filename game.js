d76b89c3b262ba596648b9a5c68e8d59c53bc2d8
saturdaaaaaaaay <ecc226@nau.edu>
2019-09-23 12:24:33 -0700

    Added NPC text and win state

--- game.js
+++ game.js
@@ -10,10 +10,30 @@ var square = new PIXI.Sprite(PIXI.Texture.fromImage("Square_Man.png"));
 var circle = new PIXI.Sprite(PIXI.Texture.fromImage("circle_dude.png"));
 var bill = new PIXI.Sprite(PIXI.Texture.fromImage("bill_cipher.png"));
 
-var starText = ["Oh, I'm sorry - I'm to cool to hang out with you."];
-var squareText = ["I don't hang out with people who don't have at least 4 corners."];
-var circleText = ["..."];
+var starText = [
+    "Oh, I'm sorry - I'm too cool to hang out with you.",
+    "Dude, like for real.",
+  ];
+var squareText = [
+  "I don't hang out with people who don't have at least 4 corners.",
+  "Ew, you only have 3 corners - I'm going to throw up.",
+  "Ugh I can't be seen with you.",
+  "Gross, I can't."
+  ];
+var circleText = "...";
 var speech = "";
+
+var starCount = 0;
+var squareCount = 0;
+var circleCount = 0;
+
+var starLimit = 2;
+var squareLimit = 4;
+var circleLimit = 10;
+
+var starFriend = false;
+var squareFriend = false;
+var circleFriend = false;
 
 stage.addChild(bill);
 bill.position.x = 200;
@@ -104,34 +124,100 @@ function animate()
   if (bill.position.x >= square.position.x - buffer &&
     bill.position.x <= square.position.x + buffer &&
     bill.position.y >= square.position.y - buffer &&
-    bill.position.y <= square.position.y + buffer)
+    bill.position.y <= square.position.y + buffer &&
+    !squareFriend)
   {
-    square.position.x = Math.floor(Math.random() * 300) + 50;
-    square.position.y = Math.floor(Math.random() * 300) + 50;
+    if (circleFriend)
+    {
+      squareFriend = true;
+      speech = "Square: Woah! You're friends with Circle? They have, like, "
+        + "infinite corners.";
+    }
+    else if (squareCount < squareLimit)
+    {
+      square.position.x = Math.floor(Math.random() * 300) + 50;
+      square.position.y = Math.floor(Math.random() * 300) + 50;
 
-    speech = "Square: " + squareText[0];
+      speech = "Square: " + squareText[squareCount];
+      squareCount += 1;
+    }
+    else
+    {
+      squareFriend = true;
+      speech = "Square: Actually, 3 corners are the new fashion. Let's be friends.";
+    }
   }
-
-  if (bill.position.x >= star.position.x - buffer &&
+  else if (bill.position.x >= star.position.x - buffer &&
     bill.position.x <= star.position.x + buffer &&
     bill.position.y >= star.position.y - buffer &&
-    bill.position.y <= star.position.y + buffer)
+    bill.position.y <= star.position.y + buffer &&
+    !starFriend)
   {
-    star.position.x = Math.floor(Math.random() * 300) + 50;
-    star.position.y = Math.floor(Math.random() * 300) + 50;
+    if (circleFriend)
+    {
+      starFriend = true;
+      speech = "Man, Circle is the coolest guy around. Maybe you are alright."
+    }
+    else if (starCount < starLimit)
+    {
+      star.position.x = Math.floor(Math.random() * 300) + 50;
+      star.position.y = Math.floor(Math.random() * 300) + 50;
 
-    speech = "Star: " + starText[0];
+      speech = "Star: " + starText[starCount];
+      starCount += 1;
+    }
+    else
+    {
+      starFriend = true;
+      speech = "Star: Who am I kidding? I don't have any friends either.";
+    }
   }
-
-  if (bill.position.x >= circle.position.x - buffer &&
+  else if (bill.position.x >= circle.position.x - buffer &&
     bill.position.x <= circle.position.x + buffer &&
     bill.position.y >= circle.position.y - buffer &&
-    bill.position.y <= circle.position.y + buffer)
+    bill.position.y <= circle.position.y + buffer &&
+    !circleFriend)
   {
-    circle.position.x = Math.floor(Math.random() * 300) + 50;
-    circle.position.y = Math.floor(Math.random() * 300) + 50;
+    if (squareFriend && starFriend)
+    {
+      circleFriend = true;
+      speech = "Circle: Your friends seem alright.";
+    }
+    else if (circleCount < circleLimit)
+    {
+      circle.position.x = Math.floor(Math.random() * 300) + 50;
+      circle.position.y = Math.floor(Math.random() * 300) + 50;
 
-    speech = "Circle: " + circleText[0];
+      speech = "Circle: " + circleText;
+      circleCount += 1;
+    }
+    else
+    {
+      circleFriend = true;
+      speech = "Circle: Fine.";
+    }
+  }
+
+  if (squareFriend)
+  {
+    square.position.x = bill.position.x + 20;
+    square.position.y = bill.position.y - 10;
+  }
+  if (starFriend)
+  {
+    star.position.x = bill.position.x - 20;
+    star.position.y = bill.position.y - 10;
+  }
+  if (circleFriend)
+  {
+    circle.position.x = bill.position.x;
+    circle.position.y = bill.position.y + 30;
+  }
+
+  if (squareFriend && starFriend && circleFriend)
+  {
+    document.getElementById("winmessage").innerHTML =
+      "Congrats! Now you have friends."
   }
 
   document.getElementById("speechtext").innerHTML = speech;
