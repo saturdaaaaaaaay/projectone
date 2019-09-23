451e43b422a214c916de2440faf789cf660d4d37
saturdaaaaaaaay <ecc226@nau.edu>
2019-09-22 21:52:26 -0700

    Character Text and Tweening

--- game.js
+++ game.js
@@ -10,34 +10,24 @@ var square = new PIXI.Sprite(PIXI.Texture.fromImage("Square_Man.png"));
 var circle = new PIXI.Sprite(PIXI.Texture.fromImage("circle_dude.png"));
 var bill = new PIXI.Sprite(PIXI.Texture.fromImage("bill_cipher.png"));
 
-/*
-var playground = new PIXI.Container();
-playground.position.x = 200;
-playground.position.y = 200;
-stage.addChild(playground);
-*/
+var starText = ["Oh, I'm sorry - I'm to cool to hang out with you."];
+var squareText = ["I don't hang out with people who don't have at least 4 corners."];
+var circleText = ["..."];
+var speech = "";
 
 stage.addChild(bill);
-//bill.anchor.x = 0.5;
-//bill.anchor.y = 0.5;
 bill.position.x = 200;
 bill.position.y = 200;
 
 stage.addChild(star);
-//star.anchor.x = 0.5;
-//star.anchor.y = 0.5;
 star.position.x = 295;
 star.position.y = 295;
 
 stage.addChild(circle);
-//circle.anchor.x = 0.5;
-//circle.anchor.y = 0.5;
 circle.position.x = 45;
 circle.position.y = 270;
 
 stage.addChild(square);
-//square.anchor.x = 0.5;
-//square.anchor.y = 0.5;
 square.position.x = 180;
 square.position.y = 90;
 
@@ -45,22 +35,60 @@ function keydownEventHandler(e)
 {
 
   var key = e.keyCode;
+  var duration = 100;
+  var leaveText = "You're not allowed to leave.";
+  var billX;
+  var billY;
 
-  if (key == 87 || key == 38) { // W key
-    bill.position.y -= 5;
+  if (key == 87 || key == 38)
+  { // W key
+    if (bill.position.y - 10 > 0)
+    {
+      billY = bill.position.y - 10;
+    }
+    else
+    {
+      speech = leaveText;
+    }
   }
 
-  if (key == 83 || key == 40) { // S key
-    bill.position.y += 5;
+  if (key == 83 || key == 40)
+  { // S key
+    if (bill.position.y + 10 < 380)
+    {
+      billY = bill.position.y + 10;
+    }
+    else
+    {
+      speech = leaveText;
+    }
   }
 
-  if (key == 65 || key == 37) { // A key
-    bill.position.x -= 5;
+  if (key == 65 || key == 37)
+  { // A key
+    if (bill.position.x - 10 > 0)
+    {
+      billX = bill.position.x - 10;
+    }
+    else
+    {
+      speech = leaveText;
+    }
   }
 
-  if (key == 68 || key == 39) { // D key
-    bill.position.x += 5;
+  if (key == 68 || key == 39)
+   { // D key
+     if (bill.position.x + 10 < 380)
+     {
+       billX = bill.position.x + 10;
+     }
+     else
+     {
+       speech = leaveText;
+     }
   }
+
+  createjs.Tween.get(bill.position).to({x: billX, y: billY}, duration);
 }
 
 document.addEventListener('keydown', keydownEventHandler);
@@ -69,7 +97,7 @@ function animate()
 {
   requestAnimationFrame(animate);
 
-  document.getElementById("billpos").innerHTML = "Bill position: " + bill.position.x + ", " + bill.position.y;
+  //document.getElementById("billpos").innerHTML = "Bill position: " + bill.position.x + ", " + bill.position.y;
 
   var buffer = 25;
 
@@ -81,7 +109,7 @@ function animate()
     square.position.x = Math.floor(Math.random() * 300) + 50;
     square.position.y = Math.floor(Math.random() * 300) + 50;
 
-    //document.getElementById("squarepos").innerHTML += "\nSquare position: " + square.position.x + ", " + square.position.y;
+    speech = "Square: " + squareText[0];
   }
 
   if (bill.position.x >= star.position.x - buffer &&
@@ -91,6 +119,8 @@ function animate()
   {
     star.position.x = Math.floor(Math.random() * 300) + 50;
     star.position.y = Math.floor(Math.random() * 300) + 50;
+
+    speech = "Star: " + starText[0];
   }
 
   if (bill.position.x >= circle.position.x - buffer &&
@@ -100,7 +130,11 @@ function animate()
   {
     circle.position.x = Math.floor(Math.random() * 300) + 50;
     circle.position.y = Math.floor(Math.random() * 300) + 50;
+
+    speech = "Circle: " + circleText[0];
   }
+
+  document.getElementById("speechtext").innerHTML = speech;
 
   renderer.render(stage);
 }
